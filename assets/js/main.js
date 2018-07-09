895398fb95961a29cd62ace192794046e0510a63
Frank FitzGerald <me@frankfitzgerald.net>
2018-07-09 14:36:35 -0400

    add type effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -246,6 +246,32 @@
 
 			});
 
+	  // H1 Type effects
+
+		function typeEffect(element, speed) {
+			var text = $(element).text();
+			$(element).html('');
+
+			var i = 0;
+			var timer = setInterval(function() {
+				if (i < text.length) {
+					$(element).append(text.charAt(i));
+					i++;
+				} else {
+					clearInterval(timer);
+				}
+			}, speed);
+		}
+
+		// Activate type typeEffect
+		var speed = 75;
+		var delay = $('h1').text().length * speed + speed;
+		typeEffect($('h1'), speed);
+		setTimeout(function(){
+			$('p').css('display', 'inline-block');
+			typeEffect($('p'), speed);
+		}, delay);
+
 		// Menu.
 			var $menu = $('#menu'),
 				$menuInner;
